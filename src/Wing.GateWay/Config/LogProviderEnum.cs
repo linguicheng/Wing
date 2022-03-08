861bb14e6f4e97807d7cfcd0c23d6118de74ae8f
linguicheng <2377087481@qq.com>
2022-03-08 22:38:56 +0800

    【网关】修改策略配置格式

---
+++ src/Wing.GateWay/Config/LogProviderEnum.cs
@@ -0,0 +1,18 @@
+﻿namespace Wing.GateWay.Config
+{
+    public enum LogProviderEnum
+    {
+        /// <summary>
+        /// Channel发布订阅
+        /// </summary>
+        Channel = 0,
+        /// <summary>
+        /// 直连数据库
+        /// </summary>
+        DataBase = 1,
+        /// <summary>
+        /// 分布式消息总线
+        /// </summary>
+        EventBus = 2
+    }
+}
