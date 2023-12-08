64a7007c91704f8a42949c3a11924e11f5e1e82b
linguicheng <773199358@qq.com>
2023-12-08 17:15:10 +0800

    网关添加聚合服务路由配置

---
+++ src/Wing.GateWay/Config/Route.cs
@@ -0,0 +1,27 @@
+﻿namespace Wing.Gateway.Config
+{
+    public class Route
+    {
+        public Upstream Upstream { get; set; }
+
+        public List<Downstream> Downstreams { get; set; }
+    }
+
+    public class Upstream
+    {
+        public string Url { get; set; }
+
+        public string Method { get; set; }
+    }
+
+    public class Downstream
+    {
+        public string ServiceName { get; set; }
+
+        public string Url { get; set; }
+
+        public string Method { get; set; }
+
+        public string Key { get; set; }
+    }
+}
