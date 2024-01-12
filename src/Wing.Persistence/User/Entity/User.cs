004516edab4084a98d0b7c895ef6a25ebe12b16b
linguicheng <773199358@qq.com>
2024-01-12 11:40:03 +0800

    用户实体

---
+++ src/Wing.Persistence/User/Entity/User.cs
@@ -0,0 +1,18 @@
+﻿using FreeSql.DataAnnotations;
+
+namespace Wing.Persistence.User
+{
+    public class User
+    {
+        [Column(IsPrimary = true)]
+        public string Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string UserAccount { get; set; }
+
+        public string Password { get; set; }
+
+        public string 
+    }
+}
