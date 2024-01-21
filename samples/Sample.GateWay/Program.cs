33c05f4cac43d2fe6ccd996c1d0734d183f2105d
linguicheng <773199358@qq.com>
2024-01-21 16:22:41 +0800

    网关jwt授权示例

--- samples/Sample.GateWay/Program.cs
+++ samples/Sample.GateWay/Program.cs
@@ -3,17 +3,17 @@ using Wing;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.AddWing(builder => builder.AddConsul());
-
-builder.Services.AddWing()
-                    .AddJwt()
-                    .AddGateWay((downstreams,context) =>
-                    {
-                        //此处添加业务授权逻辑，也可以将payload解析的内容通过请求头转发到下游服务context.Request.Headers.Add()
-                        return Task.FromResult(true);
-                    }, new WebSocketOptions
-                    {
-                        KeepAliveInterval = TimeSpan.FromMinutes(2)
-                    });
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication("Bearer").AddJwtBearer();
+builder.Services.AddWing()                
+                .AddGateWay((downstreams,context) =>
+                {
+                    //此处添加业务授权逻辑，也可以将payload解析的内容通过请求头转发到下游服务context.Request.Headers.Add()
+                    return Task.FromResult(true);
+                }, new WebSocketOptions
+                {
+                    KeepAliveInterval = TimeSpan.FromMinutes(2)
+                });
 //.AddEventBus();    
 
 var app = builder.Build();
