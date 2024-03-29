3317947ed2f4552f6cbf1bc3c52b354426a6d9e4
linguicheng <773199358@qq.com>
2024-03-29 19:07:18 +0800

    认证

--- src/Wing.UI/Controllers/BaseController.cs
+++ src/Wing.UI/Controllers/BaseController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Wing.UI.Controllers
 {
     [ApiController]
     [Route("wing/api/[controller]/[action]")]
+    [Authorize]
     public class BaseController : ControllerBase
     {
     }
