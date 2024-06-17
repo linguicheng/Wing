ece40c6773064cf671756f5c75b422d44739fb6c
linguicheng <773199358@qq.com>
2024-06-17 08:54:09 +0800

    md5加密

--- src/Wing.UI/Controllers/User/UserController.cs
+++ src/Wing.UI/Controllers/User/UserController.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Wing.Model;
-using Wing.Persistence.Saga;
 using Wing.Persistence.User;
 using Wing.Result;
+using Wing.UI.Helper;
 
 namespace Wing.UI.Controllers
 {
@@ -15,6 +16,14 @@ namespace Wing.UI.Controllers
             _userService = userService;
         }
 
+        [HttpPost]
+        [AllowAnonymous]
+        public Task<int> Login(User dto)
+        {
+            dto.Password = new Encrption().ToMd5(dto.Password);
+            return _userService.Add(dto);
+        }
+
         [HttpGet]
         public Task<PageResult<List<UserDto>>> List([FromQuery] PageModel<UserSearchDto> dto)
         {
@@ -24,12 +33,14 @@ namespace Wing.UI.Controllers
         [HttpPost]
         public Task<int> Add(User dto)
         {
+            dto.Password = new Encrption().ToMd5(dto.Password);
             return _userService.Add(dto);
         }
 
         [HttpPost]
         public Task<int> Update(User dto)
         {
+            dto.Password = new Encrption().ToMd5(dto.Password);
             return _userService.Update(dto);
         }
 
